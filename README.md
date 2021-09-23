13285c3243a81cb6af340ca805c0d45f688659c0
rinaedward <rina.eds@gmail.com>
2021-09-23 05:21:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-4mdke8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-4mdke8)
---
+++ index.ts
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+
+console.log(x);
+let x = 10;
+console.log(x)
+
+
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
