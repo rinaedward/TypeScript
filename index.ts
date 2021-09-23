1b014529514e2435e1deac1a91f68211838b6bc8
rinaedward <rina.eds@gmail.com>
2021-09-23 05:22:12 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,10 +3,6 @@ import './style.css';
 
 // Write TypeScript code!
 
-console.log(x);
-let x = 10;
-console.log(x)
-
 
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
